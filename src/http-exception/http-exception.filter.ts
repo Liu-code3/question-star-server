bbbd18f780caaa4daad9ae2fe9773c0d91f23795
webLXM <1944754630@qq.com>
2024-10-17 22:43:29 +0800

    feat(filter): 全局过滤器-AppModule

---
+++ src/http-exception/http-exception.filter.ts
@@ -0,0 +1,34 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message = exception.message
+      ? exception.message
+      : 'Internal server error';
+
+    const errorResponse = {
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      message,
+    };
+    response.status(status).json(errorResponse);
+  }
+}
