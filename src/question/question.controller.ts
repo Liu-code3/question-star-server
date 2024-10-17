8a48f04699aa171e2caa50313b2a24ce6ca986ea
webLXM <1944754630@qq.com>
2024-10-17 22:42:57 +0800

    feat(question): question模块

---
+++ src/question/question.controller.ts
@@ -0,0 +1,59 @@
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { QuestionService } from './question.service';
+import { QuestionDto } from './dto/question.dto';
+import { Question } from './entity/question.entity';
+
+@Controller('question')
+export class QuestionController {
+  constructor(private readonly questionService: QuestionService) {}
+
+  // @Get()
+  // test() {
+  //   throw new HttpException('获取数据失败', HttpStatus.BAD_REQUEST);
+  // }
+
+  @Get()
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+  ): Promise<{ records: Question[]; total: number }> {
+    return this.questionService.findAll({ keyword, page, pageSize });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') _id: number): Promise<Question> {
+    return this.questionService.findOne(_id);
+  }
+
+  @Post()
+  async create(@Body() createQuestionDto: QuestionDto): Promise<Question> {
+    return await this.questionService.create(createQuestionDto);
+  }
+
+  // @Get()
+  // findOne(
+  //   @Query('keyword') keyword: string,
+  //   @Query('page') page: number,
+  //   @Query('pageSize') size: number,
+  // ) {
+  //   return {
+  //     name: 'pangkun',
+  //     keyword,
+  //     page,
+  //     size,
+  //   };
+  // }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateQuestionDto: QuestionDto,
+  ) {
+    const { affected } = await this.questionService.update(
+      id,
+      updateQuestionDto,
+    );
+    return affected > 0 ? '更新成功' : '更新失败';
+  }
+}
---
+++ src/question/question.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Question } from './entity/question.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class QuestionService {
+  constructor(
+    @InjectRepository(Question)
+    private questionRepository: Repository<Question>,
+  ) {}
+
+  async create({ title, desc }) {
+    return await this.questionRepository.save({
+      title,
+      desc,
+    });
+  }
+
+  async delete(id: number) {
+    return await this.questionRepository.delete({ _id: id });
+  }
+
+  async findOne(id: number) {
+    return await this.questionRepository.findOne({
+      where: { _id: id },
+    });
+  }
+
+  async update(id: number, { title, desc }) {
+    return await this.questionRepository.update(id, {
+      title,
+      desc,
+    });
+  }
+
+  async findAll({ keyword = '', page = 1, pageSize = 10 }) {
+    const records = await this.questionRepository
+      .createQueryBuilder('question') // 创建查询构建器
+      .where('question.title like :keyword', { keyword: `%${keyword}%` }) // 通过 where 方法添加条件，筛选标题包含关键词的问题。
+      .orderBy('question._id', 'DESC') // 按_id降序排序
+      .skip((page - 1) * pageSize) // 分页
+      .take(pageSize) // 限制查询结果的数量为 pageSiz
+      .getMany();
+    const total = await this.questionRepository.count();
+    return {
+      page,
+      pageSize,
+      records,
+      total,
+    };
+  }
+}
