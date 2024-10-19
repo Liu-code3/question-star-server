8195d846563dbc6b2f938cb4cc1c9773a6112358
webLXM <1944754630@qq.com>
2024-10-19 14:10:01 +0800

    feat(jwt): 集成jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDto } from '../user/dto/user.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() userInfo: UserDto) {
     const { username, password } = userInfo;
 
     return await this.authService.signIn(username, password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   // 依赖注入
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(username: string, password: string) {
     const user = await this.userService.findOne(username, password);
@@ -13,6 +17,8 @@ export class AuthService {
       throw new UnauthorizedException('登录账号或者密码错误');
     }
 
-    return user;
+    return {
+      token: this.jwtService.sign({ ...user }),
+    };
   }
 }
