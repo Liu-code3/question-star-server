eca5ba564a5503feafb82fd732f127c97696259b
webLXM <1944754630@qq.com>
2024-10-19 14:17:43 +0800

    feat(authentication): 路由声明为 public 的机制

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,12 +9,14 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDto } from '../user/dto/user.dto';
+import { Public } from './decorators/public.decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @Public()
   @HttpCode(HttpStatus.OK)
   async login(@Body() userInfo: UserDto) {
     const { username, password } = userInfo;
